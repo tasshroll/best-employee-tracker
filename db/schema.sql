7af14d076a81c6d51c2f9ade41e3cfa56ae5b722
Tifni Shroll <tasshroll@gmail.com>
2023-05-08 08:15:08 -0700

    add commas for seeding employee table

--- db/schema.sql
+++ db/schema.sql
@@ -10,22 +10,24 @@ CREATE TABLE department (
 );
 
 CREATE TABLE role (
-    id  NOT NULL AUTO_INCREMENT INT PRIMARY KEY,
-	title VARCHAR(30),
-	salary INT NOT NULL,
-	department_id INT NOT NULL
+    id  INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary INT NOT NULL,
+    department_id INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
 CREATE TABLE employee (
-    id  NOT NULL AUTO_INCREMENT INT PRIMARY KEY,
-  first_name VARCHAR(30) NOT NULL,
-  last_name VARCHAR(30) NOT NULL,
-	role_id 
-	manager_id
-    FOREIGN KEY (role_id))
-    REFERENCES role(id)
-    FOREIGN KEY (manager_id)
-    REFERENCES employee (id)
+    id  INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (role_id) REFERENCES role(id),
+    FOREIGN KEY (manager_id) REFERENCES employee(id)
     ON DELETE SET NULL
-    );
+);
+
 

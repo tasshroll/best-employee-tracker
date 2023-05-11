61a858d1380f81023826b198624fed22962b3417
Tifni Shroll <tasshroll@gmail.com>
2023-05-11 09:26:46 -0700

    parse DB data after Queries. Use data to insert into manager ID

--- server.js
+++ server.js
@@ -15,22 +15,7 @@ const db = mysql.createConnection(
     console.log(`Connected to the company_db ddatabase.`)
 );
 
-// Deffault response for any other request (Not Found)
-
-
-// Hardcoded query: DELETE FROM course_names WHERE id = 3;
-// db.query(`DELETE FROM employee WHERE id = ?`, 3, (err, result) => {
-//     if (err) {
-//         console.log(err);
-//     }
-//     console.log(result);
-// });
-
-
-// Default response for any other request (Not Found)
-
-
-const company_questions = [
+const main_questions = [
     {
         type: "list",
         message: "What would you like to do?",
@@ -50,54 +35,31 @@ const company_questions = [
 
 
 const promptUser = () => {
-
     inquirer
-        .prompt(company_questions)
+        .prompt(main_questions)
         .then((answers) => {
-            // log the JSON object
-   
-            // ask more questions
             switch (answers.action) {
                 case "View All Employees":
                     displayEmp();
-                   
-
                     break;
                 case "Add Employee":
                     addEmployee();
-                    //                     What is the employee’s first name? Sam
-                    // What is the employee’s last name? Kash
-                    // What is the employee’s rolle? Customer Service
-                    // Who is the employee’s manager? Ashleigh Rodes
-                    // Added Sam Kash to the database
                     break;
                 case "Update Employee Role":
-                 
-
                     break;
                 case "View All Roles":
-                    db.query('SELECT * FROM roles;', function (err, results) {
-                        console.table(results);
-                    });
-              
-
+                    displayRoles();
                     break;
                 case "Add Role":
-                   createNewRole()
-
+                    createNewRole()
                     break;
                 case "View All Departments":
-                    db.query('SELECT * FROM department;', function (err, results) {
-                        console.table(results);
-                    });
-                  
-
+                    displayDepts();
                     break;
                 case "Add Department":
-             
 
                     break;
-                
+
                 default:
                     process.exit()
             }
@@ -113,12 +75,16 @@ function init() {
 
 
 function displayEmp() {
-    const sql = `SELECT employee.id, employee.first_name, employee.last_name, 
+    const sql = `SELECT employee.id, CONCAT(employee.first_name,' ', employee.last_name) as Employee, 
                  roles.title, roles.salary, department.dept_name, CONCAT(manager.first_name, ' ', manager.last_name) AS Manager
                  FROM employee
                  LEFT JOIN roles ON employee.role_id = roles.id
                  LEFT JOIN department ON roles.department_id = department.id
                  LEFT JOIN employee AS manager ON manager.id = employee.manager_id`;
+    // last JOIN above renamed 2nd instance of employee table as manager table
+    // foreign key (employee.manager_id)
+    // is set equal to primaary key (manager.id)
+
     db.query(sql, function (err, results) {
         console.table(results);
         promptUser()
@@ -127,119 +93,133 @@ function displayEmp() {
 
 
 function addEmployee() {
-    db.query(`SELECT first_name, last_name FROM employee;`, function (err, results) {
-        //console.log(results);
-        const emp_list = results;
-        console.table(emp_list);
-        console.log(emp_list);
-        //emp_list_to_display = 
+    db.query(`SELECT first_name, last_name, id FROM employee;`, (err, results) => {
+        console.table(results)
+        // Parse out the data
+        const empList = results.map(row => ({
+            f_name: row.first_name,
+            l_name: row.last_name,
+            id: row.id
+        }));
+        console.log(empList);
 
-        const formattedEmpResults = results.map(row => `${row.first_name} ${row.last_name}`);
+        db.query(`SELECT * from ROLES`, (err, results) => {
+            console.table(results);
+            // Parse out data
+            const roleList = results.map(row => ({
+                name: row.title,
+                salary: row.salary,
+                value: row.id
+            }));
 
-        console.log(formattedEmpResults);
-        // Output: ["John Doe", "Mike Chan", "Ashley Rodriguez", "Kevin Tupik", "Kunal Singh", "Malia Brown", "Sarah Lourd", "Tom Allen"]
+            const emp_questions = [
+                {
+                    type: "input",
+                    message: "What is the new employee's first name?",
+                    name: "f_name",
+                },
+                {
+                    type: "input",
+                    message: "What is the new employee's last name?",
+                    name: "l_name",
+                },
+                {
+                    type: "list",
+                    message: "What is the employee's role?",
+                    name: "role_title",
+                    choices: roleList.map(role => role.name)
+                },
+                {
+                    type: "list",
+                    message: "Who is the employee's manager?",
+                    name: "boss",
+                    choices: empList.map(emp => emp.f_name + ' ' + emp.l_name)
+                },
+            ];
 
-
-
-        const emp_questions = [
-            {
-                type: "input",
-                message: "What is the employee's first name?",
-                name: "f_name",
-            },
-            {
-                type: "input",
-                message: "What is the employee's last name?",
-                name: "l_name",
-            },
-            {
-                type: "list",
-                message: "What is the employee's role?",
-                name: "role_title",
-                choices: [
-                    "Sales Lead",
-                    "Sales Person",
-                    "Lead Engineer",
-                    "Software Engineer",
-                    "Account Manager",
-                    "Accountant",
-                    "Legal Team Lead",
-                    "Lawyer",
-                    "Use Arrow Keys to select"
-                ]
-            },
-            {
-                type: "list",
-                message: "Who is the employee's manager?",
-                name: "boss",
-                choices: formattedEmpResults
-            },
-        ];
-
-        inquirer.prompt(emp_questions)
-
-            .then((answers) => {
-
-
-
-
-
-
-
-                // log the JSON object
-                const { f_name, l_name, role_title, boss } = answers;
-                // Check if manager is in database
-
-                console.log(f_name, l_name, role_title, boss);
-                //INSERT INTO employee VALUES (first_name, last_name, #, #);
-                // Added Sam Kash to the database
-                //( "John", "Doe", 1, 4), -- Sales Lead
-
-                // find the index of the selected boss in formattedEmpResults
-                const roleIndex = formattedRoleResults.indexOf(role_title) + 1;
-                console.log ("The roleIndex is ", roleIndex);
-
-                const bossIndex = formattedEmpResults.indexOf(boss) + 1;
-                console.log ("The bossIndex is ", bossIndex);
-
-                // get the manager_id of the selected boss from the emp_list array
-;
-                const params = [f_name, l_name, roleIndex, bossIndex];
-                const sql = `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)`;
-                //db.query(`INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES (f_name, l_name, role_title, supervisor);`);
-                db.query(sql, params, (err, result) => {
-                    if (err) {
-                        console.log("Not added to db");
-                        return;
-                    }
-                    console.log(`Added ${f_name} ${l_name} to the database as ${role_title} with manager_id ${boss}`);
-                    console.table(result);
-                    db.query("SELECT * FROM employee");
+            inquirer.prompt(emp_questions)
+                .then((answers) => {
+                    const { f_name, l_name, role_title, boss } = answers;
+                    const role = roleList.find(role => role.name === role_title);
+                    const bossId = empList.find(emp => emp.f_name + ' ' + emp.l_name === boss).id;
+                    const params = [f_name, l_name, role.value, bossId];
+                    const sql = `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)`;
+                    db.query(sql, params, (err, result) => {
+                        if (err) {
+                            console.log("Not added to db");
+                            return;
+                        }
+                        console.log(`Added ${f_name} ${l_name} to the database as ${role_title} with manager_id ${bossId}`);
+                        promptUser();
+                    });
                 })
-            });
+                .catch(() => {
+                    console.log("Inquirer prompt failed");
+                });
+        });
+    });
+}
+function displayRoles() {
+db.query('SELECT * FROM roles;', function (err, results) {
+    console.table(results);
+    promptUser();
+});
+}
+
+function displayDepts () {
+    db.query('SELECT * FROM department;', function (err, results) {
+        if (err) {
+            console.log("Could not read from db");
+        }
+        console.table(results);
+        promptUser();
     });
 }
 
-function createNewRole(){
-    db.query('select *  from department', (err, res)=> {
-        const deptList =  res.map((dept)=> ({
-        name: dept.dept_name, 
-        value: dept.id
-        }))
+function createNewRole() {
+    // The callback function will receive an error object if the query failed, or the results of the query if it succeeded.
+    db.query('select * from department', (err, res) => {
+        // Create an array deptList, used as choices for a list prompt in Inquirer below
+        const deptList = res.map((dept) => ({
+            name: dept.dept_name,
+            value: dept.id
+        })
+        )
 
         inquirer.prompt([
-        {
-            name: 'department_id', 
-            type: 'list', 
-            message: "what department does this role belong to?", 
-            choices: deptList
+            {
+                name: 'new_role',
+                type: 'input',
+                message: 'Enter name of new Role'
+            },
+            {
+                name: 'salary',
+                type: 'input',
+                message: 'Enter salary for this role'
+            },
+            {
+                name: 'department_id',
+                type: 'list',
+                message: "What department does this role belong to?",
+                choices: deptList
+            }
+        ]).then((res) => {
+            // After user enters role, salary, and chooses a department, 
+            // insert role, salary, and department id into roles table
+            console.log(res);
+            const params = [res.new_role, res.salary, res.department_id];
+            const sql = "INSERT INTO roles (title, salary, department_id) VALUES (?, ?, ?)";
 
-        }, 
-    ]).then((res)=> {
-    console.log(res);
-})
-
-    })
-}
+            db.query(sql, params, function (err, results) {
+              if (err) {
+                console.error("Error inserting data into database:", err);
+                return;
+              }
+              console.log("Data inserted successfully:");
+              promptUser(); // Ask user for next action
+            });
+        })
+    }) // end of db.query
+}  //END of createNewRole
 
 init();
